0aac50c2360b90f8d6e4e42e055d33cfde37963f
jeonghwanJay <nawhgnoejij@naver.com>
2022-09-28 14:31:03 +0900

    add : emit Event

--- vue-cli/src/components/AppHeader.vue
+++ vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>{{propsdata}}</h1>
+    <button v-on:click="addEvent">add</button>
   </div>
 </template>
 
 <script>
 export default {
-props: ['propsdata']
+props: ['propsdata'],
+methods: {
+    addEvent: function() {
+        this.$emit('renew')
+    }
+}
 }
 </script>
 
